3d04aa8a9900a74ff54d83f74c129eeff8cad116
bz-PC\bz <bhnzhang@gmail.com>
2018-04-18 11:26:54 -0400

    finer sweep thresh 0 no plotting

--- test_scripts/s_test_synth_grating_gaussian_v2_layerratio.m
+++ test_scripts/s_test_synth_grating_gaussian_v2_layerratio.m
@@ -21,7 +21,7 @@ optimal_angle       = 20;             % still useful
 coupling_direction  = 'down';
 data_dir            = 'C:\Users\bz\Google Drive\research\popovic group\projects\grating synthesis\data';
 data_filename       = 'lol.mat';
-data_notes          = 'finer sweep with inver/normal threshold of 0.79';
+data_notes          = 'finer sweep with inver/normal threshold of 0';
 data_mode           = 'new';
 n_workers           = 1;
 
@@ -61,23 +61,23 @@ toc;
 save( [ 'synth_obj_' Q.start_time '.mat' ], 'Q', '-v7.3' );
 
 
-% directivity vs. fill
-figure;
-imagesc( Q.fill_top_bot_ratio, Q.fill_bots, 10*log10(Q.directivities_vs_fills) );
-colorbar; set( gca, 'ydir', 'normal' );
-xlabel('top/bottom fill ratio'); ylabel('bottom fill factor');
-title('Directivity (dB) vs. fill factors');
-savefig('dir_v_ff.fig');
-saveas(gcf, 'dir_v_ff.png');
-
-% directivity BEFORE sweeping periods vs. fill
-figure;
-imagesc( Q.fill_top_bot_ratio, Q.fill_bots, 10*log10(Q.dir_b4_period_vs_fills) );
-colorbar; set( gca, 'ydir', 'normal' );
-xlabel('top/bottom fill ratio'); ylabel('bottom fill factor');
-title('Directivity (dB) BEFORE PERIOD SWEEP vs. fill factors');
-savefig('dir_b4_period_v_ff.fig');
-saveas(gcf, 'dir_b4_period_v_ff.png');
+% % directivity vs. fill
+% figure;
+% imagesc( Q.fill_top_bot_ratio, Q.fill_bots, 10*log10(Q.directivities_vs_fills) );
+% colorbar; set( gca, 'ydir', 'normal' );
+% xlabel('top/bottom fill ratio'); ylabel('bottom fill factor');
+% title('Directivity (dB) vs. fill factors');
+% savefig('dir_v_ff.fig');
+% saveas(gcf, 'dir_v_ff.png');
+% 
+% % directivity BEFORE sweeping periods vs. fill
+% figure;
+% imagesc( Q.fill_top_bot_ratio, Q.fill_bots, 10*log10(Q.dir_b4_period_vs_fills) );
+% colorbar; set( gca, 'ydir', 'normal' );
+% xlabel('top/bottom fill ratio'); ylabel('bottom fill factor');
+% title('Directivity (dB) BEFORE PERIOD SWEEP vs. fill factors');
+% savefig('dir_b4_period_v_ff.fig');
+% saveas(gcf, 'dir_b4_period_v_ff.png');
 
 % % TEMP remove sat
 % figure;
@@ -90,18 +90,18 @@ saveas(gcf, 'dir_b4_period_v_ff.png');
 
 
 
-% directivity vs. fill, saturated
-dir_v_fill_sat                                  = 10*log10(Q.directivities_vs_fills);
-sat_thresh                                      = 20;                                   % threshold, in dB
-dir_v_fill_sat( dir_v_fill_sat < sat_thresh )   = sat_thresh;
-
-figure;
-imagesc( Q.fill_top_bot_ratio, Q.fill_bots, dir_v_fill_sat );
-colorbar; set( gca, 'ydir', 'normal' );
-xlabel('top/bottom fill ratio'); ylabel('bottom fill factor');
-title('Directivity (dB) (saturated) vs. fill factors');
-savefig([ 'dir_v_ff_sat_' num2str(sat_thresh) '.fig']);
-saveas(gcf, [ 'dir_v_ff_sat_' num2str(sat_thresh) '.png']);
+% % directivity vs. fill, saturated
+% dir_v_fill_sat                                  = 10*log10(Q.directivities_vs_fills);
+% sat_thresh                                      = 20;                                   % threshold, in dB
+% dir_v_fill_sat( dir_v_fill_sat < sat_thresh )   = sat_thresh;
+% 
+% figure;
+% imagesc( Q.fill_top_bot_ratio, Q.fill_bots, dir_v_fill_sat );
+% colorbar; set( gca, 'ydir', 'normal' );
+% xlabel('top/bottom fill ratio'); ylabel('bottom fill factor');
+% title('Directivity (dB) (saturated) vs. fill factors');
+% savefig([ 'dir_v_ff_sat_' num2str(sat_thresh) '.fig']);
+% saveas(gcf, [ 'dir_v_ff_sat_' num2str(sat_thresh) '.png']);
 
 
 % % plot the way jelena did
@@ -155,158 +155,158 @@ saveas(gcf, [ 'dir_v_ff_sat_' num2str(sat_thresh) '.png']);
 % title('Directivity (dB) (only datapoints on inverted curve) vs. fill factors');
 
 
-% angles vs. fill
-figure;
-imagesc( Q.fill_top_bot_ratio, Q.fill_bots, Q.angles_vs_fills );
-colorbar; set( gca, 'ydir', 'normal' );
-xlabel('top/bottom fill ratio'); ylabel('bottom fill factor');
-title('Angles (deg) vs. fill factors');
-savefig('angle_v_ff.fig');
-saveas(gcf, 'angle_v_ff.png');
-
-% scattering strength alpha vs. fill
-figure;
-imagesc( Q.fill_top_bot_ratio, Q.fill_bots, real(Q.scatter_str_vs_fills) );
-colorbar; set( gca, 'ydir', 'normal' );
-xlabel('top/bottom fill ratio'); ylabel('bottom fill factor');
-title('Scattering strength (real) vs. fill factors');
-savefig('scatter_str_v_ff.fig');
-saveas(gcf, 'scatter_str_v_ff.png');
-
-% period vs. fill
-figure;
-imagesc( Q.fill_top_bot_ratio, Q.fill_bots, Q.periods_vs_fills );
-colorbar; set( gca, 'ydir', 'normal' );
-xlabel('top/bottom fill ratio'); ylabel('bottom fill factor');
-title(['Period (' Q.units.name ') vs. fill factors']);
-savefig('period_v_ff.fig');
-saveas(gcf, 'period_v_ff.png');
-
-% offset vs. fill
-figure;
-imagesc( Q.fill_top_bot_ratio, Q.fill_bots, Q.offsets_vs_fills );
-colorbar; set( gca, 'ydir', 'normal' );
-xlabel('top/bottom fill ratio'); ylabel('bottom fill factor');
-title('Offset vs. fill factors');
-savefig('offsets_v_ff.fig');
-saveas(gcf, 'offsets_v_ff.png');
-
-% k vs. fill
-figure;
-imagesc( Q.fill_top_bot_ratio, Q.fill_bots, real(Q.k_vs_fills) );
-colorbar; set( gca, 'ydir', 'normal' );
-xlabel('top/bottom fill ratio'); ylabel('bottom fill factor');
-title('Real k vs. fill factors');
-savefig('k_real_v_ff.fig');
-saveas(gcf, 'k_real_v_ff.png');
-
-figure;
-imagesc( Q.fill_top_bot_ratio, Q.fill_bots, imag(Q.k_vs_fills) );
-colorbar; set( gca, 'ydir', 'normal' );
-xlabel('top/bottom fill ratio'); ylabel('bottom fill factor');
-title('Imag k vs. fill factors');
-savefig('k_imag_v_ff.fig');
-saveas(gcf, 'k_imag_v_ff.png');
-
-
-% offset (jelena's definition) vs. fills
-offset_jelena = Q.offsets_vs_fills + ...
-                repmat(Q.fill_bots.', 1, length(Q.fill_top_bot_ratio) ) - ...
-                repmat(Q.fill_bots.', 1, length(Q.fill_top_bot_ratio) ) .* repmat(Q.fill_top_bot_ratio, length(Q.fill_bots), 1);
-offset_jelena = mod( offset_jelena, 1.0 );
-figure;
-imagesc( Q.fill_top_bot_ratio, Q.fill_bots, offset_jelena );
-colorbar; set( gca, 'ydir', 'normal' );
-xlabel('top/bottom fill ratio'); ylabel('bottom fill factor');
-title('Offset (jelena''s def) vs. fill factors');
-savefig('offsets_jelena_v_ff.fig');
-saveas(gcf, 'offsets_jelena_v_ff.png');
+% % angles vs. fill
+% figure;
+% imagesc( Q.fill_top_bot_ratio, Q.fill_bots, Q.angles_vs_fills );
+% colorbar; set( gca, 'ydir', 'normal' );
+% xlabel('top/bottom fill ratio'); ylabel('bottom fill factor');
+% title('Angles (deg) vs. fill factors');
+% savefig('angle_v_ff.fig');
+% saveas(gcf, 'angle_v_ff.png');
+% 
+% % scattering strength alpha vs. fill
+% figure;
+% imagesc( Q.fill_top_bot_ratio, Q.fill_bots, real(Q.scatter_str_vs_fills) );
+% colorbar; set( gca, 'ydir', 'normal' );
+% xlabel('top/bottom fill ratio'); ylabel('bottom fill factor');
+% title('Scattering strength (real) vs. fill factors');
+% savefig('scatter_str_v_ff.fig');
+% saveas(gcf, 'scatter_str_v_ff.png');
+% 
+% % period vs. fill
+% figure;
+% imagesc( Q.fill_top_bot_ratio, Q.fill_bots, Q.periods_vs_fills );
+% colorbar; set( gca, 'ydir', 'normal' );
+% xlabel('top/bottom fill ratio'); ylabel('bottom fill factor');
+% title(['Period (' Q.units.name ') vs. fill factors']);
+% savefig('period_v_ff.fig');
+% saveas(gcf, 'period_v_ff.png');
+% 
+% % offset vs. fill
+% figure;
+% imagesc( Q.fill_top_bot_ratio, Q.fill_bots, Q.offsets_vs_fills );
+% colorbar; set( gca, 'ydir', 'normal' );
+% xlabel('top/bottom fill ratio'); ylabel('bottom fill factor');
+% title('Offset vs. fill factors');
+% savefig('offsets_v_ff.fig');
+% saveas(gcf, 'offsets_v_ff.png');
+% 
+% % k vs. fill
+% figure;
+% imagesc( Q.fill_top_bot_ratio, Q.fill_bots, real(Q.k_vs_fills) );
+% colorbar; set( gca, 'ydir', 'normal' );
+% xlabel('top/bottom fill ratio'); ylabel('bottom fill factor');
+% title('Real k vs. fill factors');
+% savefig('k_real_v_ff.fig');
+% saveas(gcf, 'k_real_v_ff.png');
+% 
+% figure;
+% imagesc( Q.fill_top_bot_ratio, Q.fill_bots, imag(Q.k_vs_fills) );
+% colorbar; set( gca, 'ydir', 'normal' );
+% xlabel('top/bottom fill ratio'); ylabel('bottom fill factor');
+% title('Imag k vs. fill factors');
+% savefig('k_imag_v_ff.fig');
+% saveas(gcf, 'k_imag_v_ff.png');
+% 
+% 
+% % offset (jelena's definition) vs. fills
+% offset_jelena = Q.offsets_vs_fills + ...
+%                 repmat(Q.fill_bots.', 1, length(Q.fill_top_bot_ratio) ) - ...
+%                 repmat(Q.fill_bots.', 1, length(Q.fill_top_bot_ratio) ) .* repmat(Q.fill_top_bot_ratio, length(Q.fill_bots), 1);
+% offset_jelena = mod( offset_jelena, 1.0 );
+% figure;
+% imagesc( Q.fill_top_bot_ratio, Q.fill_bots, offset_jelena );
+% colorbar; set( gca, 'ydir', 'normal' );
+% xlabel('top/bottom fill ratio'); ylabel('bottom fill factor');
+% title('Offset (jelena''s def) vs. fill factors');
+% savefig('offsets_jelena_v_ff.fig');
+% saveas(gcf, 'offsets_jelena_v_ff.png');
 
 
 % plot of the final designs
-
-% final synthesized fills
-figure;
-plot( 1:length(Q.bot_fill_synth), Q.bot_fill_synth, '-o' ); hold on;
-plot( 1:length(Q.top_bot_fill_ratio_synth), Q.top_bot_fill_ratio_synth, '-o' );
-xlabel('cell #');
-legend('bottom fill ratio', 'top/bottom fill ratio');
-title('Final synthesized fill factor');
-makeFigureNice();
-savefig('final_fill_ratios.fig');
-saveas(gcf, 'final_fill_ratios.png');
-
-% final synthesized fills
-figure;
-plot( 1:length(Q.bot_fill_synth), Q.bot_fill_synth.*Q.period_synth, '-o' ); hold on;
-plot( 1:length(Q.top_bot_fill_ratio_synth), Q.top_bot_fill_ratio_synth.*Q.bot_fill_synth.*Q.period_synth, '-o' );
-xlabel('cell #');
-legend('bottom fill', 'top fill');
-title('Final synthesized fills (nm)');
-makeFigureNice();
-savefig('final_fills.fig');
-saveas(gcf, 'final_fills.png');
-
-% final synthesized offset
-figure;
-plot( 1:length(Q.offset_synth), Q.offset_synth, '-o' );
-xlabel('cell #');
-legend('offset ratio');
-title('Final synthesized offset ratio');
-makeFigureNice();
-savefig('final_offsets.fig');
-saveas(gcf, 'final_offsets.png');
-
-% final synthesized period
-figure;
-plot( 1:length(Q.period_synth), Q.period_synth, '-o' );
-xlabel('cell #');
-legend(['period, ' units]);
-title('Final synthesized period');
-makeFigureNice();
-savefig('final_periods.fig');
-saveas(gcf, 'final_periods.png');
-
-% final synthesized angles
-figure;
-plot( 1:length(Q.angles_synth), Q.angles_synth, '-o' );
-xlabel('cell #');
-legend('angle, deg');
-title('Final synthesized angles');
-makeFigureNice();
-savefig('final_angles.fig');
-saveas(gcf, 'final_angles.png');
-
-% final synthesized scattering strength
-figure;
-plot( 1:length(Q.scatter_str_synth), Q.scatter_str_synth, '-o' );
-xlabel('cell #');
-legend(['scattering strength (units 1/' units]);
-title('Final synthesized scattering strength');
-makeFigureNice();
-savefig('final_scattering_str.fig');
-saveas(gcf, 'final_scattering_str.png');
-
-% final synthesized scattering strength normalized comparison
-figure;
-plot( 1:length(Q.scatter_str_synth), Q.des_scatter_norm, '-o' ); hold on;
-plot( 1:length(Q.scatter_str_synth), Q.scatter_str_synth./abs(max(Q.scatter_str_synth(:))), '-o' );
-xlabel('cell #');
-legend('desired scatter str', 'normalized scatter str');
-title('Final synthesized scattering strength, normalized comparison');
-makeFigureNice();
-savefig('final_scattering_str_norm.fig');
-saveas(gcf, 'final_scattering_str_norm.png');
-
-% final synthesized directivity
-figure;
-plot( 1:length(Q.dir_synth), 10*log10(Q.dir_synth), '-o' );
-xlabel('cell #');
-legend('Directivity, dB');
-title('Final synthesized directivity');
-makeFigureNice();
-savefig('final_dir.fig');
-saveas(gcf, 'final_dir.png');
+% 
+% % final synthesized fills
+% figure;
+% plot( 1:length(Q.bot_fill_synth), Q.bot_fill_synth, '-o' ); hold on;
+% plot( 1:length(Q.top_bot_fill_ratio_synth), Q.top_bot_fill_ratio_synth, '-o' );
+% xlabel('cell #');
+% legend('bottom fill ratio', 'top/bottom fill ratio');
+% title('Final synthesized fill factor');
+% makeFigureNice();
+% savefig('final_fill_ratios.fig');
+% saveas(gcf, 'final_fill_ratios.png');
+% 
+% % final synthesized fills
+% figure;
+% plot( 1:length(Q.bot_fill_synth), Q.bot_fill_synth.*Q.period_synth, '-o' ); hold on;
+% plot( 1:length(Q.top_bot_fill_ratio_synth), Q.top_bot_fill_ratio_synth.*Q.bot_fill_synth.*Q.period_synth, '-o' );
+% xlabel('cell #');
+% legend('bottom fill', 'top fill');
+% title('Final synthesized fills (nm)');
+% makeFigureNice();
+% savefig('final_fills.fig');
+% saveas(gcf, 'final_fills.png');
+% 
+% % final synthesized offset
+% figure;
+% plot( 1:length(Q.offset_synth), Q.offset_synth, '-o' );
+% xlabel('cell #');
+% legend('offset ratio');
+% title('Final synthesized offset ratio');
+% makeFigureNice();
+% savefig('final_offsets.fig');
+% saveas(gcf, 'final_offsets.png');
+% 
+% % final synthesized period
+% figure;
+% plot( 1:length(Q.period_synth), Q.period_synth, '-o' );
+% xlabel('cell #');
+% legend(['period, ' units]);
+% title('Final synthesized period');
+% makeFigureNice();
+% savefig('final_periods.fig');
+% saveas(gcf, 'final_periods.png');
+% 
+% % final synthesized angles
+% figure;
+% plot( 1:length(Q.angles_synth), Q.angles_synth, '-o' );
+% xlabel('cell #');
+% legend('angle, deg');
+% title('Final synthesized angles');
+% makeFigureNice();
+% savefig('final_angles.fig');
+% saveas(gcf, 'final_angles.png');
+% 
+% % final synthesized scattering strength
+% figure;
+% plot( 1:length(Q.scatter_str_synth), Q.scatter_str_synth, '-o' );
+% xlabel('cell #');
+% legend(['scattering strength (units 1/' units]);
+% title('Final synthesized scattering strength');
+% makeFigureNice();
+% savefig('final_scattering_str.fig');
+% saveas(gcf, 'final_scattering_str.png');
+% 
+% % final synthesized scattering strength normalized comparison
+% figure;
+% plot( 1:length(Q.scatter_str_synth), Q.des_scatter_norm, '-o' ); hold on;
+% plot( 1:length(Q.scatter_str_synth), Q.scatter_str_synth./abs(max(Q.scatter_str_synth(:))), '-o' );
+% xlabel('cell #');
+% legend('desired scatter str', 'normalized scatter str');
+% title('Final synthesized scattering strength, normalized comparison');
+% makeFigureNice();
+% savefig('final_scattering_str_norm.fig');
+% saveas(gcf, 'final_scattering_str_norm.png');
+% 
+% % final synthesized directivity
+% figure;
+% plot( 1:length(Q.dir_synth), 10*log10(Q.dir_synth), '-o' );
+% xlabel('cell #');
+% legend('Directivity, dB');
+% title('Final synthesized directivity');
+% makeFigureNice();
+% savefig('final_dir.fig');
+% saveas(gcf, 'final_dir.png');
 
 
 % -------------------------------------------------------------------------
