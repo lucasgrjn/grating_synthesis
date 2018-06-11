2d001fd44937cd0350a0cf3cafdd3977dfc2de1d
bz <bhnzhang@gmail.com>
2018-06-11 10:47:30 -0400

    re-organizing grating cell classes

---
+++ main/c_GratingCell.m
@@ -0,0 +1,12 @@
+classdef c_GratingCell < c_bloch_cell
+    %UNTITLED Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+    end
+    
+    methods
+    end
+    
+end
+
